27facce6d6a614e554728d8e46f90c53604e6640
Abhijeet <abhijeet.songara@nectarinfotel.com>
2022-01-04 04:48:08 -0800

    Profile screen design added

---
+++ app/src/main/java/com/herbal/herbalfax/common_screen/profile/ProfileActivity.java
@@ -0,0 +1,16 @@
+package com.herbal.herbalfax.common_screen.profile;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.herbal.herbalfax.R;
+
+public class ProfileActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_profile);
+    }
+}
