363646e2817c8c6f596855b62914fc4d6c23fc34
Abhijeet <abhijeet.songara@nectarinfotel.com>
2022-01-24 20:58:33 -0800

    vendar profile

---
+++ app/src/main/java/com/herbal/herbalfax/vendor/sellerdrivers/SellerDriverDetails.java
@@ -0,0 +1,16 @@
+package com.herbal.herbalfax.vendor.sellerdrivers;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.herbal.herbalfax.R;
+
+public class SellerDriverDetails extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_seller_driver_details);
+    }
+}
