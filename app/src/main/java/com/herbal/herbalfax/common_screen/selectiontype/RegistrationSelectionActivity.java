a797e99353560d3fc55adae8e14f08cc7fb6f856
kushpandy <kush.pandya@nectarinfotel.com>
2022-02-11 17:40:12 +0530

    Client changes

--- app/src/main/java/com/herbal/herbalfax/common_screen/selectiontype/RegistrationSelectionActivity.java
+++ app/src/main/java/com/herbal/herbalfax/common_screen/selectiontype/RegistrationSelectionActivity.java
@@ -9,7 +9,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
-import android.widget.Button;
+
 
 import com.herbal.herbalfax.R;
 import com.herbal.herbalfax.common_screen.login.LoginActivity;
