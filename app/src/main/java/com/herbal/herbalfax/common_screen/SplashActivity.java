c7e125620c01eb746aee12d2f2eb28c9bedb7404
kushpandy <kush.pandya@nectarinfotel.com>
2022-02-15 18:20:57 +0530

    1. working on android issue

--- app/src/main/java/com/herbal/herbalfax/common_screen/SplashActivity.java
+++ app/src/main/java/com/herbal/herbalfax/common_screen/SplashActivity.java
@@ -5,9 +5,12 @@ import android.os.Bundle;
 import android.os.Handler;
 import android.util.Log;
 import android.view.WindowManager;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+
+import com.herbal.herbalfax.BuildConfig;
 import com.herbal.herbalfax.R;
 
 import com.herbal.herbalfax.common_screen.landingpage.CommonLandingActivity;
@@ -22,6 +25,8 @@ public class SplashActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
         // Session class instance
+        TextView versionTxt=findViewById(R.id.versionTxt);
+        versionTxt.setText(BuildConfig.VERSION_NAME);
         session = SessionPref.getInstance(this);
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
         Handler handler = new Handler();
