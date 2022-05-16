4995eb4b8f50e893d320e58ba00cdeca288d44f5
Abhijeet <abhijeet.songara@nectarinfotel.com>
2022-05-16 06:01:16 -0700

    screen design

---
+++ app/src/main/java/com/herbal/herbalfax/NewCustomerSignUpScTen.java
@@ -0,0 +1,14 @@
+package com.herbal.herbalfax;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class NewCustomerSignUpScTen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_your_business);
+    }
+}
---
+++ app/src/main/java/com/herbal/herbalfax/NewVendorSignUpScOne.java
@@ -0,0 +1,14 @@
+package com.herbal.herbalfax;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class NewVendorSignUpScOne extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_new_vendor_sign_up_sc_one);
+    }
+}
