ca0892669ce14e0f2857a8389f28890cc94ec283
Abhijeet <abhijeet.songara@nectarinfotel.com>
2022-06-06 22:43:26 -0700

    pending details screen design

---
+++ app/src/main/java/com/herbal/herbalfax/PendingdetailActivity.java
@@ -0,0 +1,14 @@
+package com.herbal.herbalfax;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class PendingdetailActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_pendingdetail);
+    }
+}
